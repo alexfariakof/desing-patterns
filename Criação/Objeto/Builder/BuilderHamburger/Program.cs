7001c94582bb79cff4f6d9974673a43e3af10792
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-07-18 18:51:05 -0300

    Exemplo de implementação Padrão Builder

---
+++ Criação/Objeto/Builder/BuilderHamburger/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using BuilderHamburger.Builer;
+
+HamburguerBuilder hamburguerBuilder = new();
+
+var xSalada  = hamburguerBuilder
+    .SetTipoPao("Australiano")
+    .SetTipoCarne("Normal")
+    .TemSalada().Build();
+
+Console.WriteLine(xSalada.GetDescricao());
+
+hamburguerBuilder = new();
+var xBacon = hamburguerBuilder
+        .SetTipoPao("Pimenta")
+        .SetTipoCarne("Frando")
+        .TemQueijo()
+        .TemBacon()
+        .Build();
+
+Console.WriteLine(xBacon.GetDescricao());
+
+hamburguerBuilder = new();
+var xTudo = hamburguerBuilder
+        .SetTipoPao("Normal")
+        .SetTipoCarne("Duas Bovina")
+        .TemQueijo()
+        .TemBacon()
+        .TemBatataPalha()
+        .TemSalada()
+        .Build();
+Console.WriteLine(xTudo.GetDescricao());

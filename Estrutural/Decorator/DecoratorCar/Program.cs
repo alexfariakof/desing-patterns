e32161239fa8f618dfc729fb0fba88a2b336feb8
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-07-28 12:51:03 -0300

    Exemplo de Implementação Padrão Decorator

--- Estrutural/Decorator/DecoratorCar/Program.cs
+++ Estrutural/Decorator/DecoratorCar/Program.cs
@@ -1,7 +1,13 @@
 ﻿
 using DecoratorCar;
 using DecoratorCar.Decorator.Concrete;
+/*
+    Dinamicamente, agregar responsabilidades adicionais a objetos.
+ Os decorators fornecem uma alternativa flexível ao uso de 
+subclasses para extensão de funcionalidades.
 
+
+ */
 ICarro carro = new Carro();
 ICarro arCondicionado = new ArCondicionadoDecorator(carro);
 ICarro bancoCouro = new BancoCouroDecorator(arCondicionado);

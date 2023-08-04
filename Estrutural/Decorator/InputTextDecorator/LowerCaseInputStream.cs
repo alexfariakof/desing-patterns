e21612463a2d995219fe7fb3aed7fb6b622e4b99
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-08-04 06:57:59 -0300

    Exemplo System.IO Decorator

---
+++ Estrutural/Decorator/InputTextDecorator/LowerCaseInputStream.cs
@@ -0,0 +1,31 @@
+﻿namespace InputTextDecorator
+{
+    public class LowerCaseInputStream : Stream
+    {
+        private Stream baseStream;
+        public LowerCaseInputStream(Stream baseStream)
+        {
+            this.baseStream = baseStream;
+        }
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int bytesRead = baseStream.Read(buffer, offset, count);
+            System.Globalization.TextInfo textInfo = System.Globalization.CultureInfo.CurrentCulture.TextInfo;
+            for (int i = 0; i < bytesRead; i++)
+            {
+                buffer[offset + i] = (byte)textInfo.ToLower((char)buffer[offset + i]);
+            }
+
+            return bytesRead;
+        }
+        public override void Write(byte[] buffer, int offset, int count) { }
+        public override void Flush() { }
+        public override long Seek(long offset, SeekOrigin origin) { throw new NotImplementedException(); }
+        public override void SetLength(long value) { throw new NotImplementedException(); }
+        public override bool CanRead => true;
+        public override bool CanSeek => false;
+        public override bool CanWrite => false;
+        public override long Length => this.baseStream.Length;
+        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ Estrutural/Decorator/InputTextDecorator/Program.cs
@@ -0,0 +1,48 @@
+﻿using InputTextDecorator;
+using System.Text;
+
+string filePath = $"{GetRootPath()}\\testDecoratorStream.txt";
+Console.WriteLine("Hello, World! Input Text Decorator");
+Console.WriteLine("");
+
+using (Stream inputText =
+    new LowerCaseInputStream(
+    new BufferedStream(
+        new FileStream(filePath, FileMode.Open)
+        )))
+{
+    try
+    {
+        byte[] buffer = new byte[inputText.Length];
+        int bytesRead = inputText.Read(buffer, 0, buffer.Length);
+
+        //string fileContent = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim('\uFEFF');
+        string isocontent = Encoding.GetEncoding("ISO-8859-1").GetString(buffer, 0, bytesRead);
+        byte[] isobytes = Encoding.GetEncoding("ISO-8859-1").GetBytes(isocontent);
+        byte[] ubytes = Encoding.Convert(Encoding.GetEncoding("ISO-8859-1"), Encoding.Unicode, isobytes);
+        string fileContent = Encoding.Unicode.GetString(ubytes, 0, ubytes.Length);
+        Console.WriteLine("File Content:");
+        Console.WriteLine(fileContent);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Arquivo não pode ser lido!");
+
+    }
+    finally
+    {
+        inputText.Close();
+    }
+};
+
+static string GetRootPath()
+{
+    string currentDirectory = Directory.GetCurrentDirectory();
+
+    while (!File.Exists(Path.Combine(currentDirectory, "InputTextDecorator.csproj")))
+    {
+        currentDirectory = Directory.GetParent(currentDirectory).FullName;
+    }
+
+    return currentDirectory;
+}
